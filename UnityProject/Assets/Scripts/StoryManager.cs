423cee579f9b774e1fc77d55b88b64d60842412c
miloravi <57906122+miloravi@users.noreply.github.com>
2024-10-24 21:35:28 +0200

    Set the start to introduction instead of coffee

--- UnityProject/Assets/Scripts/StoryManager.cs
+++ UnityProject/Assets/Scripts/StoryManager.cs
@@ -49,7 +49,7 @@ public partial class StoryManager : MonoBehaviour
     private CharacterStruct activeCharacter;
     private CharacterStruct sideCharacter;
 
-    private StoryPoint storyPoint = StoryPoint.Coffee;
+    private StoryPoint storyPoint = StoryPoint.Introduction;
     private int progress = 0;
 
     public void Start()
